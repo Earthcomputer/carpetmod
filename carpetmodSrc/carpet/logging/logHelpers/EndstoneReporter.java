eee6c982c20a1875ab170a17f8644cc6d48da049
Earthcomputer <burtonjae@hotmail.co.uk>
2018-09-10 02:51:43 +0100

    Add gateway reporter

---
+++ carpetmodSrc/carpet/logging/logHelpers/EndstoneReporter.java
@@ -0,0 +1,38 @@
+package carpet.logging.logHelpers;
+
+import carpet.logging.LoggerRegistry;
+import carpet.utils.Messenger;
+import net.minecraft.entity.Entity;
+import net.minecraft.tileentity.TileEntityEndGateway;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.text.ITextComponent;
+
+public class EndstoneReporter
+{
+
+    public static void onGatewayTeleport(TileEntityEndGateway gateway, Entity entity, BlockPos targetPos, boolean generated)
+    {
+        if (LoggerRegistry.__endGateway)
+        {
+            LoggerRegistry.getLogger("endGateway").log((option) -> {
+                if ("generated".equals(option) && !generated)
+                    return null;
+                return new ITextComponent[] {
+                        Messenger.m(null,
+                                "g  tick ", "w " + gateway.getWorld().getTotalWorldTime(),
+                                "g  gateway at ", String.format("w (%d, %d, %d)", gateway.getPos().getX(), gateway.getPos().getY(), gateway.getPos().getZ()),
+                                "g  teleports to ", String.format("w (%d, %d, %d)", targetPos.getX(), targetPos.getY(), targetPos.getZ()))
+                };
+            },
+            "GATEWAY_X", gateway.getPos().getX(),
+            "GATEWAY_Y", gateway.getPos().getY(),
+            "GATEWAY_Z", gateway.getPos().getZ(),
+            "ENTITY", entity.getCachedUniqueIdString(),
+            "TARGET_X", targetPos.getX(),
+            "TARGET_Y", targetPos.getY(),
+            "TARGET_Z", targetPos.getZ(),
+            "GENERATED", generated);
+        }
+    }
+    
+}
