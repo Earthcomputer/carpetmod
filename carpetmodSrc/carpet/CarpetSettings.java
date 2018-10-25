08ff34aa4732eeff53a317698ee0ff0862a460ae
Earthcomputer <burtonjae@hotmail.co.uk>
2018-10-25 09:48:10 +0100

    Bump version to 18_10_25_earth

--- carpetmodSrc/carpet/CarpetSettings.java
+++ carpetmodSrc/carpet/CarpetSettings.java
@@ -44,7 +44,7 @@ public class CarpetSettings
     public static boolean locked = false;
 
     // TODO: replace these constants at build time
-    public static final String carpetVersion = "v18_10_16_earth";
+    public static final String carpetVersion = "v18_10_25_earth";
     public static final String minecraftVersion = "1.12";
     public static final String mcpMappings = "20180713-1.12";
 
